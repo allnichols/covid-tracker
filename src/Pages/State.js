707b12c82e600ec88e50c392b980943df32af649
mnichols <mnichols@industrialinfo.com>
2021-05-06 08:35:20 -0500

    added color for risk level and updated packages

--- src/Pages/State.js
+++ src/Pages/State.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Text, Flex, Box, Link } from '@chakra-ui/react';
 import { useLocation } from 'react-router-dom';
 
 const State = () => {
-    let {pathname} = useLocation();
+    let { pathname } = useLocation();
     
     return ( 
         <div>
-            <h1>{pathname}</h1>
+            <Text
+            as="h1"
+            textTransform="capitalize"
+            fontWeight="bold"
+            fontSize="2rem">
+                {pathname.replace('/', '')}
+            </Text>
         </div>
      );
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { InputGroup, Input, InputRightElement, Text, Flex, Box, Link } from '@chakra-ui/react';
+import { InputGroup, Input, InputRightElement, Text, Flex, Link, Box  } from '@chakra-ui/react';
 import { SearchIcon, ChevronRightIcon } from '@chakra-ui/icons';
 import { Link as ReactRouter } from "react-router-dom";
 
@@ -53,47 +53,45 @@ const SearchBar = () => {
       {filtered.map( item => {
 
         let stateName = item.url.replaceAll('https://covidactnow.org/us/', '')
+        let riskLevelNum = item.riskLevels.overall;
 
         return (
-          <Link as={ReactRouter} width="95%" to={stateName.substr(0, stateName.length - 3).replaceAll('_', ' ')}>
-            <Flex 
-              alignItems="center"
-              marginTop="16px"
-              padding="16px"
-              border="1px solid #e2e8f0"
-              borderRadius="20px"
-              width="95%"
-              justifyContent="space-between"
-              key={item.state}
-            >
-            <Flex
-              alignItems="center"
-            >
-              <Text marginRight="16px">
-                {item.riskLevels.overall}
-              </Text>
-            <Flex
-              flexDirection="column"
-            >
-              <Text
-                textTransform="capitalize"
-                fontWeight="bold"
-                fontSize="1.125rem"
-              >
-                    {stateName.substr(0, stateName.length - 3).replaceAll('_', ' ')}
-                </Text>
-                <Text>
-                Cases: {item.actuals.cases}
-                </Text>
+            <Link as={ReactRouter} width="95%" to={stateName.substr(0, stateName.length - 3).replaceAll('_', ' ')} key={item.state}>
+              <Flex 
+                alignItems="center"
+                marginTop="16px"
+                padding="16px"
+                border="1px solid #e2e8f0"
+                borderRadius="20px"
+                width="95%"
+                justifyContent="space-between">
+                <Flex alignItems="center">
+                  <Box 
+                    bg={`${riskLevel(riskLevelNum)}`} 
+                    w={15} 
+                    h={15} 
+                    borderRadius={50}
+                    marginRight="16px" />
+                  <Flex
+                    flexDirection="column"
+                  >
+                    <Text
+                      textTransform="capitalize"
+                      fontWeight="bold"
+                      fontSize="1.125rem"
+                    >
+                        {stateName.substr(0, stateName.length - 3).replaceAll('_', ' ')}
+                    </Text>
+                    <Text>
+                      {item.population} population
+                    </Text>
 
-            </Flex>
-              
-            </Flex>
-              
-              <ChevronRightIcon w={25} h={25}/>
-            </Flex>
-          </Link>
-           
+                  </Flex>
+                  
+                </Flex>
+                <ChevronRightIcon w={25} h={25}/>
+              </Flex>
+            </Link>    
           )
       })}
     </>
@@ -101,3 +99,21 @@ const SearchBar = () => {
 }
  
 export default SearchBar;
+
+function riskLevel(num) {
+  switch (num) {
+    case 0:
+      return "#00d474";
+    case 1:
+      return "#ffc900";
+    case 2:
+      return "#ff9600";
+    case 3:
+      return "#d9002c";
+    case 4:
+      return "#790019";
+    default:
+      break;
+  
+  }
+}
