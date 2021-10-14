937bdd530d9390824cdfe1634491dde44da2dc03
unknown <allenmaxnichols@gmail.com>
2021-10-13 23:39:00 -0500

    added counties-albers-10m

--- src/components/map/useData.js
+++ src/components/map/useData.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { json } from 'd3';
 
-const statesUrl = 'https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json'
+const statesUrl = 'https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json';
+const countiesUrl = 'https://cdn.jsdelivr.net/npm/us-atlas@3/counties-10m.json';
+const both = 'https://cdn.jsdelivr.net/npm/us-atlas@3/counties-albers-10m.json';
 
 export const useData = () => {
     const [data, setData] = useState(null);
     
 
     useEffect(() => {
-        json(statesUrl).then( topology => console.log(topology))
+        json(both).then( topology => console.log(topology))
     }, []);
 
     return data;
