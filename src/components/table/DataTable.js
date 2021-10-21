77500e19ad7f5421cfa1cd6d88825445cae08007
mnichols <mnichols@industrialinfo.com>
2021-10-21 15:54:01 -0500

    begun table

---
+++ src/components/table/DataTable.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+  } from "@chakra-ui/react"
+
+  const DataTable = (data) => {
+      return ( 
+            <Table varient="simple">
+
+            </Table>
+       );
+  }
+   
+  export default DataTable;
